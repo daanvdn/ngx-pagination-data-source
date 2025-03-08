36d8e5e484f87de65366c267ec6eb36568385ecc
daanv <daanvdn@gmail.com>
2025-03-08 14:38:33 +0100

    upgraded to typescript 4.9.3

---
+++ do-upgrade.ps1
@@ -0,0 +1,7 @@
+#assign first parameter to version variable
+$version = $args[0]
+
+echo "upgrading to version $version of @angular/core and @angular/cli  packages"
+ng update "@angular/core@$version" "@angular/cli$version" --create-commits
+echo "upgrading to version $version of @angular-eslint packages"
+ng update "@angular-eslint/builder@$version" "@angular-eslint/eslint-plugin@$version" "@angular-eslint/eslint-plugin-template@$version" "@angular-eslint/schematics@$version" "@angular-eslint/template-parser@$version" --create-commits
